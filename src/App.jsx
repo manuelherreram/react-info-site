83d4726c88e18d88c8912b811026c258d8b232f9
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-02-18 23:29:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar.jsx";
+import Main from "./components/Main.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
